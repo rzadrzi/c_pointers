03e6c2ba49e948298cc9cd681928723bd3c786bf
Reza Darzi <darzi.reza@gmail.com>
2025-12-29 13:20:46 +0330

    feat: Add sum of array function

--- array.c
+++ array.c
@@ -2,11 +2,24 @@
 #include <limits.h>
 #include <stdio.h>
 
+int sum_array(int arr[], int size) {
+  int sum = 0;
+  int i;
+  for (i = 0; i < size; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
 int main() {
-  int grades[3];
-  // grades = {1, 2, 3};
-  //
+  int grades[3] = {1, 2, 3};
+
   printf("%d bits\n", (int)(CHAR_BIT * sizeof(void *)));
   printf("%ld\n", sizeof(int));
+
+  int sum = sum_array(grades, 3);
+
+  printf("sum of value: %d\n", sum);
+
   return 0;
 }
