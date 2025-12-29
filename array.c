d9aab5ef940566b24143518a49d1d792084a8a76
Reza Darzi <darzi.reza@gmail.com>
2025-12-29 13:24:33 +0330

    feat: Add sum of array by pointers function

--- array.c
+++ array.c
@@ -10,6 +10,14 @@ int sum_array(int arr[], int size) {
   }
   return sum;
 }
+int sum_array_by_pointer(int *ptr, int size) {
+  int sum = 0;
+  int i;
+  for (i = 0; i < size; i++) {
+    sum = sum + ptr[i];
+  }
+  return sum;
+}
 
 int main() {
   int grades[3] = {1, 2, 3};
@@ -18,8 +26,10 @@ int main() {
   printf("%ld\n", sizeof(int));
 
   int sum = sum_array(grades, 3);
-
   printf("sum of value: %d\n", sum);
+
+  int sum_by_ptr = sum_array_by_pointer(grades, 3);
+  printf("sum of values using pointer: %d\n", sum_by_ptr);
 
   return 0;
 }
